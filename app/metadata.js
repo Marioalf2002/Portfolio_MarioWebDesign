10b247249866fa6276863dea77631907d30e7bdf
Mario Hernandez <marioalf200216@gmail.com>
2024-08-22 20:33:01 -0500

    Update MetaData

--- app/metadata.js
+++ app/metadata.js
@@ -6,21 +6,21 @@ export const metadata = {
     history: "Historia | MarioWebDesign",
     work: "Proyectos | MarioWebDesign",
     contact: "Contacto | MarioWebDesign",
-    terms: "Términos y Condiciones | MarioWebDesign",
+    terms: "Términos & Condiciones | MarioWebDesign",
   },
   description: {
     default:
       "¡Hola! Soy Mario Hernández, un desarrollador web Full-Stack con una sólida experiencia en una variedad de tecnologías. Mi pasión radica en crear soluciones web innovadoras & funcionales que satisfagan las necesidades de mis clientes.",
     home: "¡Hola! Soy Mario Hernández, un desarrollador web Full-Stack con una sólida experiencia en una variedad de tecnologías. Mi pasión radica en crear soluciones web innovadoras & funcionales que satisfagan las necesidades de mis clientes.",
     services:
-      "Descubre los servicios de desarrollo web que ofrezco, desde diseño UI/UX hasta SEO & mantenimiento. Esto& aquí para a&udarte a alcanzar tus objetivos en línea de manera efectiva & eficiente.",
+      "Descubre los servicios de desarrollo web que ofrezco, desde diseño UI/UX hasta SEO & mantenimiento. Esto& aquí para ayudarte a alcanzar tus objetivos en línea de manera efectiva & eficiente.",
     history:
-      "Conoce mi historia & tra&ectoria como desarrollador web, desde mis inicios en la programación hasta mi experiencia profesional & los proyectos más destacados en los que he trabajado.",
+      "Conoce mi historia & trayectoria como desarrollador web, desde mis inicios en la programación hasta mi experiencia profesional & los proyectos más destacados en los que he trabajado.",
     work: "Explora los proyectos de desarrollo web que he realizado, desde sitios web & aplicaciones hasta tiendas en línea. Esto& seguro de que encontrarás inspiración & calidad en mi trabajo.",
     contact:
       "Contáctame para discutir tus ideas & necesidades. Juntos, crearemos una solución web personalizada que se adapte a tus objetivos & presupuesto.",
     terms:
-      "Lee los términos & condiciones de uso de este sitio web, incluyendo la política de privacidad & cookies. Al navegar por este sitio, aceptas estos términos.",
+      "Lee los Términos & Condiciones de uso de MarioWebDesign para comprender cómo se recopila, utiliza & protege tu información personal en este sitio web.",
   },
   url: {
     default: "https://mariowebdesigns.com",
